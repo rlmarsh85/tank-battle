5bafeae9e4185cb8b1fb842bf27a70d29b679c16
Lee Marsh <rlmarsh85@gmail.com>
2019-12-29 19:26:30 -0700

    Adding force, friction material

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,18 +2,19 @@
 
 
 #include "Public/TankTrack.h"
-
+#include "Components/PrimitiveComponent.h" 
 
 
 
 void UTankTrack::SetThrottle(float Throttle) {
-	UE_LOG(LogTemp, Warning, TEXT("Throttle on track: %f"), Throttle);
+	UE_LOG(LogTemp, Warning, TEXT("Throttle on track: %f"), Throttle)
 
 	/// TODO: Clamp input value
 
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
 
-	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	UPrimitiveComponent* TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());	
+	UE_LOG(LogTemp, Warning, TEXT("%s component being moved with %s force: "), *TankRoot->GetName(),*ForceApplied.ToString())
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
